31355619f53096cbed6bb563fc23c787329fec91
Juan L. Sánchez <juanlu-28a@hotmail.com>
2015-12-02 19:43:31 +0100

    Visualizar receta
    Se ha creado una vista para visualizar una receta. Con esto se ha creado una directiva para mostrar las etiquetas.

---
+++ project/src/main/webapp/scripts/directives/tags-directive.js
@@ -0,0 +1,14 @@
+/**
+ * Created by juanlu on 2/12/15.
+ */
+
+angular.module('chefsApp').directive('tags', function()
+{
+    return {
+        restrict: 'A',
+        templateUrl: 'scripts/directives/tags-template.html',
+        scope: {
+            tags:"=tagsList"
+        }
+    }
+});
