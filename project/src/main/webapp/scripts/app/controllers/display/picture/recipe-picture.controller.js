2d176f53c302cdc7257f4812bbb6b227ce0f104e
Juan L. Sánchez <juanlu-28a@hotmail.com>
2016-01-18 21:44:25 +0100

    Creado listado de imagenes en front-end
    Se ha creado el listado de imagenes, se esta trabajando en la redirección hacia la receta.
    Se debe de configurar para ver las imagenes del id del usuario de la url.

---
+++ project/src/main/webapp/scripts/app/controllers/display/picture/recipe-picture.controller.js
@@ -0,0 +1,76 @@
+'use strict';
+
+angular.module('chefsApp')
+    .controller('RecipePicturesController', function ($scope, RecipeUser, ParseLinks) {
+        $scope.pictures = [];
+        $scope.pictureDetails = {};
+        $scope.pictureIndex = 0;
+        $scope.page = 0;
+        $scope.loadAll = function() {
+            RecipeUser.get({page: $scope.page, size: 10}, function(result, headers) {
+                $scope.links = ParseLinks.parse(headers('link'));
+                for (var i = 0; i < result.length; i++) {
+                    if(result[i].socialEntity.socialPicture.src != null) {
+                        $scope.pictures.push({
+                            src: result[i].socialEntity.socialPicture.src, recipe: result[i].id,
+                            recipeName: result[i].name, title: result[i].socialEntity.socialPicture.title,
+                            updateDate: result[i].updateDate, class: 'col-lg-4'
+                        });
+                    }
+                    for (var j = 0; j < result[i].steps.length; j++ ){
+                        for (var k = 0; k < result[i].steps[j].stepPicture.length; k++){
+                            if(result[i].steps[j].stepPicture[k].src != null) {
+                                $scope.pictures.push({
+                                    src: result[i].steps[j].stepPicture[k].src, recipe: result[i].id,
+                                    recipeName: result[i].name, title: result[i].steps[j].stepPicture[k].title,
+                                    updateDate: result[i].updateDate, class: 'col-lg-4'
+                                });
+                            }
+                        }
+                    }
+                }
+            });
+        };
+        $scope.reset = function() {
+            $scope.page = 0;
+            $scope.pictures = [];
+            $scope.loadAll();
+        };
+        $scope.loadPage = function(page) {
+            $scope.page = page;
+            $scope.loadAll();
+        };
+        $scope.loadAll();
+
+        $scope.refresh = function () {
+            $scope.reset();
+            $scope.clear();
+        };
+
+        $scope.detailsOfPicture = function(pictureIndex){
+            $scope.pictureDetails = $scope.pictures[pictureIndex];
+            $scope.pictureIndex = pictureIndex;
+        };
+
+        $scope.nextPicture = function(){
+            if ($scope.pictureIndex < $scope.pictures.length - 1 ){
+                $scope.pictureIndex = $scope.pictureIndex + 1;
+                $scope.pictureDetails = $scope.pictures[$scope.pictureIndex];
+            }
+        };
+
+        $scope.prevPicture = function(){
+            if ($scope.pictureIndex > 0 ){
+                $scope.pictureIndex = $scope.pictureIndex - 1;
+                $scope.pictureDetails = $scope.pictures[$scope.pictureIndex];
+            }
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+        $scope.clear = function () {
+            $scope.pictures = {src:null, recipe:null, recipeName:null, title:null, updateDate:null};
+        };
+    });
---
+++ project/src/main/webapp/scripts/app/controllers/display/picture/recipe-picture.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('chefsApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('home_recipe-pictures', {
+                parent: 'display',
+                url: '/recipe-pictures/{id}',
+                data: {
+                    pageTitle: 'chefsApp.recipe.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/views/picture/recipe-pictures.html',
+                        controller: 'RecipePicturesController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('recipe');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
