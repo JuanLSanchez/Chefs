6ef1ed17b23ada6222b3f8b3f59904e1bf39b9c4
Juan L. Sánchez <juanlu-28a@hotmail.com>
2016-05-12 21:52:03 +0200

    Inicio de manual de usuario
    Se esta creando un apartado para el manual del usuario. Se ha iniciado la página, la cual estará realizada en AngularJs

---
+++ project/src/main/webapp/scripts/app/controllers/display/info/info.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('chefsApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('info', {
+                parent: 'display',
+                url: '/info',
+                data: {
+                    pageTitle: 'chefsApp.recipe.home.title'
+                },
+                views: {
+                    'content_2@': {
+                        templateUrl: 'scripts/app/views/info/info.html'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('info');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
